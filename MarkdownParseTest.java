4331f62db5b1316c66889958cd7ca9578b93b20a
Ivana <iroque1008@gmail.com>
2022-01-27 15:09:35 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,20 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLinks() throws IOException() {
+        MarkdownParse.getLinks("test-file.md");
+        assertEquals(List.of("https://something.com", "some-page.html")
+        , MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
     }
 }
