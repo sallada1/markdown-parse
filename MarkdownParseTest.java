46d2e7d07f0ddb95481fe1ea95889fd3e86fefbb
sallada1 <sallada@ucsd.edu>
2022-01-28 09:27:52 -0800

    IndexOutOfBounds exception addressed

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,20 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+    @Test
+    public void testGetLinks() throws IOException{
+        assertEquals(List.of("https://something.com","some-page.html"),MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
+    }
+    
+    
 }
