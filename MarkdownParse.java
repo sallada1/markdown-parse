addf8bfa989d2c702eefc22397cb852e488ebb78
sallada1 <sallada@ucsd.edu>
2022-02-11 13:34:29 -0800

    adding a line

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,11 +15,11 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if(nextOpenBracket > 0 && markdown.charAt(nextOpenBracket - 1) != '!'){
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-            }
             if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1){ 
                 break;
+            }
+            if(nextOpenBracket > 0 && markdown.charAt(nextOpenBracket - 1) != '!'){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             currentIndex = closeParen + 1;
         }
